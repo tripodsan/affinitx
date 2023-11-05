c16c0331f5e336e1a6d25e7664eda1f0550ed16c
Tobias Bocanegra <tripod@adobe.com>
2023-11-05 23:17:30 +0100

    chore: add pedestal

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+butler push export/web tripod/scale-game:html5
+butler push export/macos/scale-game.dmg tripod/scale-game:macos
---
+++ scenes/pedestal.gd
@@ -0,0 +1,15 @@
+@tool
+extends StaticBody3D
+
+class_name Pedestal
+
+@onready var cube = $cube_container/cube
+
+@export var preview:bool = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+  if preview or not Engine.is_editor_hint():
+    cube.rotate_x(0.5 * delta)
+    cube.rotate_y(0.6 * delta)
+    cube.rotate_z(0.7 * delta)
