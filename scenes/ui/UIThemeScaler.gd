a4723f7597adefb7aa68c82e315e321e3b8532f1
Tobias Bocanegra <tripod@adobe.com>
2023-11-24 14:08:46 +0100

    add lock icon

--- scenes/ui/UIThemeScaler.gd
+++ scenes/ui/UIThemeScaler.gd
@@ -7,14 +7,14 @@ const META_NAME = 'initial_font_size'
 
 @export var tracked_themes:Array[Theme] = []
 
-func _ready():
-  get_viewport().size_changed.connect(adjust_font_size)
-  for theme in tracked_themes:
-    theme.set_meta(META_NAME, theme.get_font_size('default', 'default'))
-  adjust_font_size()
-
-func adjust_font_size()->void:
-  var ui_scale:float = Utils.get_viewport_scale(self)
-  for theme in tracked_themes:
-    theme.set_default_font_size(theme.get_meta(META_NAME) * ui_scale)
+#func _ready():
+#  get_viewport().size_changed.connect(adjust_font_size)
+#  for theme in tracked_themes:
+#    theme.set_meta(META_NAME, theme.get_font_size('default', 'default'))
+#  adjust_font_size()
+#
+#func adjust_font_size()->void:
+#  var ui_scale:float = Utils.get_viewport_scale(self)
+#  for theme in tracked_themes:
+#    theme.set_default_font_size(theme.get_meta(META_NAME) * ui_scale)
 
--- scenes/ui/hud.gd
+++ scenes/ui/hud.gd
@@ -2,9 +2,11 @@ extends Control
 
 @onready var gun_ui_grow = %grow
 @onready var gun_ui_shrink = %shrink
+@onready var lock = %lock
 
 func _ready():
   Global.weapon_change.connect(_on_weapon_change)
+  Global.target_lock_changed.connect(_on_lock_changed)
 
 func _on_weapon_change(gun:Gun):
   if !gun:
@@ -14,3 +16,6 @@ func _on_weapon_change(gun:Gun):
 
   gun_ui_grow.visible = gun.beam_mode == Global.BEAM_MODE.GROW
   gun_ui_shrink.visible = gun.beam_mode == Global.BEAM_MODE.SHRINK
+
+func _on_lock_changed(enabled:bool):
+  lock.visible = enabled
