192d491163dc993796dbcb8af3d83f35ba5c969f
Tobias Bocanegra <tripod@adobe.com>
2023-11-28 18:21:32 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-butler push export/web tripod/affinitx-game:html5
+butler push export/web tripod/affinitx:html5
 butler push export/macos tripod/affinitx:macos
 butler push export/linux tripod/affinitx:linux
 butler push export/windows tripod/affinitx:windows
